9bd5f5a85939f9437e57daccc9ea4c1b91e3d0b1
TAM-Snappy <89431079+TAM-Snappy@users.noreply.github.com>
2022-06-26 17:14:22 +0200

    Update table.sh

--- table.sh
+++ table.sh
@@ -9,76 +9,78 @@ echo $ippublic
 
 while IFS= read -r table; do
 
-echo "Vérification de la table - $table"
+	echo "Vérification de la table - $table"
 
 
 
-nas="/mnt/nasdata/tables/$table/"
-repbkp=$(ls $nas -t | head -n 1)
-lastbkp=$(ls $nas"$repbkp")
+	nas="/mnt/nasdata/tables/$table/"
+	repbkp=$(ls $nas -t | head -n 1)
+	lastbkp=$(ls $nas"$repbkp")
 
 
-#DUMP DE BD
-echo "debug 0"
-ssh $sshopts ubuntu@$ippublic 'sudo mysqldump sample $table < /dev/null > $table.sql'
+	#DUMP DE BD
+	echo "debug 0"
+	ssh $sshopts ubuntu@$ippublic 'sudo mysqldump sample $table < /dev/null > $table.sql'
 
-sleep 10
+	sleep 10
 
-scp $sshopts ubuntu@$ippublic:/home/ubuntu/$table.sql . < /dev/null
+	scp $sshopts ubuntu@$ippublic:/home/ubuntu/$table.sql . < /dev/null
 
 
-echo "debug 1"
+	echo "debug 1"
 
-if ! [ -d $nas ]; then
+	if ! [ -d $nas ]; then
        
     
-    echo "aucune backup de la table -$table, lancement d'une sauvegarde"
+   	 	echo "aucune backup de la table -$table, lancement d'une sauvegarde"
     
-	mkdir -p $nas"$(date)" 
-	cp $table.sql $nas"$(date)"/$table.sql 
-	echo "sauvegarde de la table - $table terminée"
+		mkdir -p $nas"$(date)" 
+		cp $table.sql $nas"$(date)"/$table.sql 
+		echo "sauvegarde de la table - $table terminée"
 
 
-else
+	else
 
 
 	#VERIFICATION DE CHANGEMENT DANS BD AVANT DEPLACEMENT DANS LE NAS
 
 
-	nb_ligne1=$(cat $table.sql | wc -l)
-	echo "debut 2"
-    	nb_ligne2=$(cat "$nas$repbkp/$lastbkp" | wc -l)
-	echo "debut 3"
-	var1=$(($nb_ligne1 - 1))
-	echo "debut 4"
-	head -n $var1 $table.sql > save_${table}1.sql
-	echo "debut 5"
-	var2=$(($nb_ligne2 - 1))
-	echo "debut 6"
-	head -n $var2 "$nas$repbkp/$lastbkp" > save_${lastbkp}2.sql
-	echo "debut 7"
-	sv1=$(md5sum save_${table}1.sql | cut -d ' ' -f1)
-	sv2=$(md5sum save_${lastbkp}2.sql | cut -d ' ' -f1)
-	echo "debut 8"
+		nb_ligne1=$(cat $table.sql | wc -l)
+		echo "debut 2"
+    		nb_ligne2=$(cat "$nas$repbkp/$lastbkp" | wc -l)
+		echo "debut 3"
+		var1=$(($nb_ligne1 - 1))
+		echo "debut 4"
+		head -n $var1 $table.sql > save_${table}1.sql
+		echo "debut 5"
+		var2=$(($nb_ligne2 - 1))
+		echo "debut 6"
+		head -n $var2 "$nas$repbkp/$lastbkp" > save_${lastbkp}2.sql
+		echo "debut 7"
+		sv1=$(md5sum save_${table}1.sql | cut -d ' ' -f1)
+		sv2=$(md5sum save_${lastbkp}2.sql | cut -d ' ' -f1)
+		echo "debut 8"
 
-	if [[ $sv1 == $sv2 ]]; then
+		if [[ $sv1 == $sv2 ]]; then
 
-		echo "aucune nouvelle modification sur la table - $table"
+			echo "aucune nouvelle modification sur la table - $table"
+			continue
 
-	else
+		else
 
-		#DEPLACEMENT DANS LE NAS
+			#DEPLACEMENT DANS LE NAS
 		
-		echo "modification detecté sur la table - $table"
-		mkdir -p $nas"$(date)" 
-		cp sample.sql $nas"$(date)/$table.sql" 
-		echo "sauvegarde de la table - $table terminée"
+			echo "modification detecté sur la table - $table"
+			mkdir -p $nas"$(date)" 
+			cp sample.sql $nas"$(date)/$table.sql" 
+			echo "sauvegarde de la table - $table terminée"
 
+		fi
+    	echo "debug 9"
+   	
+	
 	fi
-    
-   
-fi
-
+echo "debug 10"
 
 done < <(ssh $sshopts ubuntu@$ippublic 'sudo mysql -e "use sample; show tables \G" | grep "Tables" | cut -d " " -f2' < /dev/null)
 
